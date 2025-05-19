755a9f61018fd9e4f74f93137280e8528f9d2fd9
feg55 <andhkj@gmail.com>
2025-05-19 21:30:28 +0300

    beta-commit jast for checking 2

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,11 +1,12 @@
 import name from "../src/cli.js";
 import readlineSync from 'readline-sync';
-let mass = [];
+
 const isEven = (num) => num % 2 === 0;
 const isOdd = (num) => num % 2 !== 0;
 
+console.log('Answer "yes" if the number is even, otherwise answer "no".');
+
 const eaven = () => {
-    console.log('Answer "yes" if the number is even, otherwise answer "no".');
 
     let num = Math.floor(Math.random() * 99) + 1;
     let corrAnswer;
@@ -22,21 +23,14 @@ const eaven = () => {
 
     if (isOdd(num) && answer === "no") {
         console.log("Correct!");
-        mass.push("")
     } else if (isEven(num) && answer === "yes") {
         console.log("Correct!");
-        mass.push("")
     } else {
         console.log(`${answer} is wrong answer ;( Correct answer was ${corrAnswer}. Let's try again, ${name}`);
     }
 };
-const isEvenGame = () => {
-for (let i = 0; mass.length < 3; i++){
-eaven();
-}
-console.log("Congratulations, " + name + "!")
-}
 
-export {isEvenGame}
+console.log("Congratulations, " + name + "!")
+eaven()
 export default eaven;
 
