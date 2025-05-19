bc4584ff7249b560a82968068308497264b7bf79
feg55 <andhkj@gmail.com>
2025-05-19 21:56:21 +0300

    14 commit

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,11 +1,10 @@
 import name from "../src/cli.js";
 import readlineSync from 'readline-sync';
-
+let sum = 0;
 const isEven = (num) => num % 2 === 0;
 const isOdd = (num) => num % 2 !== 0;
 
 console.log('Answer "yes" if the number is even, otherwise answer "no".');
-
 const eaven = () => {
 
     let num = Math.floor(Math.random() * 99) + 1;
@@ -26,11 +25,19 @@ const eaven = () => {
     } else if (isEven(num) && answer === "yes") {
         console.log("Correct!");
     } else {
-        console.log(`${answer} is wrong answer ;( Correct answer was ${corrAnswer}. Let's try again, ${name}`);
+        console.log(`${answer} is wrong answer ;( Correct answer was "${corrAnswer}". Let's try again, ${name}`);
+        sum = sum + 1
     }
 };
 
-console.log("Congratulations, " + name + "!")
 eaven()
-export default eaven;
+eaven()
+eaven()
 
+if (sum === 0) {
+console.log("Congratulations, " + name + "!")
+} else {
+console.log("I know you can better)))")
+}
+
+export default eaven;
